b05dbfb19eebab1e128f54932302c05f0904b4d4
David Salinas <saotand@gmail.com>
2022-06-07 04:24:11 -0400

    Login routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Registro de usuarios
+Route::post('register', [AuthController::class , 'registro']);
+
+// Logueo de Usuarios
+Route::post('login', [AuthController::class , 'acceso']);
+
+Route::get('/',function (){
+    return 'none';
 });
