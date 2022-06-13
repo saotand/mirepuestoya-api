19e5bcded91b2551dd115a877d663be462694e7e
David Salinas <saotand@gmail.com>
2022-06-13 00:33:27 -0400

    add routes for brands

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CarBrandController;
+use App\Models\CarBrand;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,10 +31,23 @@ Route::post('register', [AuthController::class , 'registro']);
 Route::post('login', [AuthController::class , 'acceso']);
 
 // Logout de Usuarios
-Route::middleware('auth:sanctum')->post('logout', [AuthController::class , 'salida']);
+Route::post('logout', [AuthController::class , 'salida'])->middleware('auth:sanctum');
+
+
+// Rutas para formualrio de inicio (home)
+
+    // CarBrands
+    Route::get('carbrands', [CarBrandController::class,'index_home']);
 
 // Grupo de Rutas con Token
 Route::group(['middleware'=> ['auth:sanctum']], function (){
+
+    Route::get('admin/carbrands',[CarBrandController::class,'index']);
+    Route::post('admin/carbrands',[CarBrandController::class,'store']);
+    Route::get('admin/carbrands/{carbrand}',[CarBrandController::class,'show']);
+    Route::put('admin/carbrands/{carbrand}',[CarBrandController::class,'update']);
+    Route::delete('admin/carbrands/{carbrand}',[CarBrandController::class,'destroy']);
+
 
 });
 
