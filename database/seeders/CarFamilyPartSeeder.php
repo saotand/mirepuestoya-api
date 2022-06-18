c19a6bf6a17e2327c626b3323d342f1c5a363d6e
David Salinas <saotand@gmail.com>
2022-06-18 14:54:00 -0400

    Create seeder faker array

--- database/seeders/CarFamilyPartSeeder.php
+++ database/seeders/CarFamilyPartSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CarFamilyPartSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class CarFamilyPartSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('car_brands')->insert([
+            [
+                "name"=>"",
+                "image"=>NULL,
+                "description"=> NULL,
+            ]
+        ]);
     }
 }
