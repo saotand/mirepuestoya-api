47911b23208311aeff262c1c1a29693a7b57f0ff
David Salinas <saotand@gmail.com>
2022-08-01 15:40:11 -0400

    Folder API

---
+++ app/Http/Resources/CarBrandResourceSelect.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+
+class CarBrandResourceSelect extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //Default Definition
+        //return parent::toArray($request);
+
+        // Select structure
+        return [
+            'value'=>$this->id,
+            'text'=>Str::of($this->name)->upper()
+        ];
+
+    }
+}
