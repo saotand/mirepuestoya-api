8bb265aae10fbbd505270ac29b88231aa6b782a0
David Salinas <saotand@gmail.com>
2022-09-10 10:29:36 -0400

    Resource for model's model

---
+++ app/Http/Resources/API/CarBrandResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+// use Illuminate\Support\Str;
+
+class CarBrandResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // Defaul Assign
+        // return parent::toArray($request);
+
+
+        // Estructura para definicion
+        return [
+
+        ];
+
+    }
+}
---
+++ app/Http/Resources/API/CarModelResourceSelect.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+
+class CarModelResourceSelect extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //Default Definition
+        //return parent::toArray($request);
+
+        // Select structure
+        return [
+            'value'=>$this->id,
+            'text'=>Str::of($this->name)->upper()
+        ];    }
+}
