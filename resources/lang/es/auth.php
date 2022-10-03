9ae18d72009ae25e10494c1669187932fc7401d5
David Salinas <saotand@gmail.com>
2022-10-03 12:41:57 -0400

    Minor Changes

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'Las credenciales no coinciden con nuestros registros.',
+    'password'=>'La contraseña proporcionada no es la correcta',
     'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
 
 ];
