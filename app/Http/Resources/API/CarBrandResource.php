62424504916c3f217ea5ccb763cb0a264cad3347
David Salinas <saotand@gmail.com>
2022-10-03 12:40:40 -0400

    Resource for CarBrand

--- app/Http/Resources/API/CarBrandResource.php
+++ app/Http/Resources/API/CarBrandResource.php
@@ -18,11 +18,23 @@ class CarBrandResource extends JsonResource
         // Defaul Assign
         // return parent::toArray($request);
 
-
-        // Estructura para definicion
-        return [
-
+        $resourceHolder =  [
+            'name'=>$this->name,
+            'description' => $this->description,
+            'counter'=> $this->counter,
+            'meta'=>[
+                'active'=> $this->active,
+            ]
         ];
-
+        if($this->created_at){
+            $resourceHolder['meta']['created'] = $this->created_at;
+        }
+        if($this->updated_at){
+            $resourceHolder['meta']['created'] = $this->updated_at;
+        }
+        if($this->image){
+            $resourceHolder['meta']['image'] = $this->image;
+        }
+        return $resourceHolder;
     }
 }
